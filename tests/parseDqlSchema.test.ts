f9237665329e3f2813224b5e770fa67161212e13
Bastin <BastinJafari@gmail.com>
2023-05-27 14:17:12 +0200

    refactor: tests

--- tests/parseDqlSchema.test.ts
+++ tests/parseDqlSchema.test.ts
@@ -1,66 +1,14 @@
 import { parseDqlSchema } from "../src/utils";
-
-const testSchema = `# Define Types
-
-type Person {
-    name
-    boss_of
-    works_for
-}
-
-type Company {
-    name
-    industry
-    work_here #this is an alias
-}
-
-# Define Directives and index
-
-industry: string @index(term) .
-boss_of: [uid] .
-name: string @index(exact, term) .
-works_for: [uid] .
-work_here: [uid] .`;
-
-console.log(JSON.stringify(parseDqlSchema(testSchema), null, 2));
+const testSchemas = require("./testSchemas.json");
 
 describe("parseDqlSchema", () => {
-  it("should return a DQLSchema object", () => {
-    expect(parseDqlSchema(testSchema)).toEqual({
-      types: [
-        {
-          name: "Person",
-          fields: ["name", "boss_of", "works_for"],
-        },
-        {
-          name: "Company",
-          fields: ["name", "industry", "work_here"],
-        },
-      ],
-      directives: [
-        {
-          name: "industry",
-          type: "string",
-          index: ["term"],
-        },
-        {
-          name: "boss_of",
-          type: "[uid]",
-        },
-        {
-          name: "name",
-          type: "string",
-          index: ["exact", "term"],
-        },
-        {
-          name: "works_for",
-          type: "[uid]",
-        },
-        {
-          name: "work_here",
-          type: "[uid]",
-        },
-      ],
-    });
+  it("should return a correct DQLSchema object for every DQLSchema", () => {
+    for (const testSchema of testSchemas) {
+      console.log(JSON.stringify(parseDqlSchema(testSchema.dqlSchemaString), null, 2));
+
+      expect(parseDqlSchema(testSchema.dqlSchemaString)).toEqual(
+        testSchema.dqlSchemaParsed
+      );
+    }
   });
 });
