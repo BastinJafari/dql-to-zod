a12523697ae00c5c7503652d98dcd7a73964e123
Bastin <BastinJafari@gmail.com>
2023-05-27 19:55:43 +0200

    refactor: put all test data into json file

--- tests/generateZodSchema.test.ts
+++ tests/generateZodSchema.test.ts
@@ -2,71 +2,19 @@
 
 import { generateZodSchema } from "../src/generateZodSchema"
 import fs from "fs"
-
+const testSchemas = require("./testSchemas.json")
 describe("generateZodSchema", () => {
   //check if a generated zod schema is in file generatedZodSchema.ts
-  it("should generate a file", async () => {
-    const zodSchemaString = `import { z } from 'zod'
-
-    const Person = z.object({
-      name: z.string(),
-      boss_of: z.array(z.string().uuid()).optional(),
-      works_for: z.array(z.string().uuid()).optional(),
-    })
-
-    type Person = z.infer<typeof Person>
+  it("should save the correct zod schemas and types to the file system for all schemas", async () => {
     
-    const Company = z.object({
-      name: z.string(),
-      industry: z.string(),
-      work_here: z.array(z.string().uuid()).optional(),
+    for (const testSchema of testSchemas) {
+
+        generateZodSchema(testSchema.dqlSchemaParsed)
+        const data = await fs.promises.readFile(
+            "src/generatedZodSchema.ts",
+            "utf8"
+          )
+          expect(data.replace(/\s/g, "")).toEqual(testSchema.zodSchemaString.replace(/\s/g, ""))
+    }
     })
-    
-    type Company = z.infer<typeof Company>
-    `
-
-    const schema = generateZodSchema({
-      types: [
-        {
-          name: "Person",
-          fields: ["name", "boss_of", "works_for"],
-        },
-        {
-          name: "Company",
-          fields: ["name", "industry", "work_here"],
-        },
-      ],
-      directives: [
-        {
-          name: "industry",
-          type: "string",
-          index: ["term"],
-        },
-        {
-          name: "boss_of",
-          type: "[uid]",
-        },
-        {
-          name: "name",
-          type: "string",
-          index: ["exact", "term"],
-        },
-        {
-          name: "works_for",
-          type: "[uid]",
-        },
-        {
-          name: "work_here",
-          type: "[uid]",
-        },
-      ],
-    })
-
-    const data = await fs.promises.readFile(
-      "src/generatedZodSchema.ts",
-      "utf8"
-    )
-
-    expect(data.replace(/\s/g, "")).toEqual(zodSchemaString.replace(/\s/g, ""))
-  })
 })
