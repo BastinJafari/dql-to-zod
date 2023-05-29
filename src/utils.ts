8c6b6cdc03891077dc62851ec88f2c591cd288cd
Bastin <BastinJafari@gmail.com>
2023-05-29 21:26:29 +0200

    feat: improve parser

--- src/utils.ts
+++ src/utils.ts
@@ -1,67 +1,85 @@
-import { DQLSchema, Directive, Type } from "./types"
+import { DQLSchema, Directive, Type } from "./types";
 
 export const removeCommentsOutOfLine = (line: string): string => {
   if (line.startsWith("#")) {
-    return ""
+    return "";
   } else if (line.includes("#")) {
-    return line.split("#")[0]
+    return line.split("#")[0];
   } else {
-    return line
+    return line;
   }
-}
+};
 
 export const parseDqlSchema = (dqlSchemaString: string): DQLSchema => {
   const schema: DQLSchema = {
     types: [],
     directives: [],
-  }
+  };
 
   // Split the schema into lines
-  let lines = dqlSchemaString.split("\n")
+  let lines = dqlSchemaString.split("\n");
 
   //delete comments
-  lines = lines.map((line) => removeCommentsOutOfLine(line))
+  lines = lines.map((line) => removeCommentsOutOfLine(line));
   //delete empty lines
-  lines = lines.filter((line) => line !== "")
+  lines = lines.filter((line) => line !== "");
   //delete empty spaces
-  lines = lines.map((line) => line.trim())
+  lines = lines.map((line) => line.trim());
 
-  console.log(lines)
+  let loopIsInDgraphType = false;
+  for (let n = 0; n < lines.length; n++) {
+    //remove lines that include dgraph types
+    if (lines[n].startsWith("type dgraph")) {
+      loopIsInDgraphType = true;
+      continue;
+    }
+    if (loopIsInDgraphType) {
+      if (lines[n].startsWith("}")) {
+        loopIsInDgraphType = false;
+        continue;
+      }
+      continue;
+    }
 
-  for (let n = 0; n < lines.length ;n++) {
+    //remove lines that include dgraph directives
+    if (lines[n].startsWith("dgraph")) {
+      console.log("removing line with dgraph directive:", lines[n]);
+      continue;
+    }
+
     if (lines[n].startsWith("type")) {
       const type: Type = {
         name: lines[n].split(" ")[1],
         fields: [],
-      }
+      };
       for (let i = n + 1; i < lines.length; i++) {
         if (lines[i].startsWith("}")) {
-          n = i
-          break
+          n = i;
+          break;
         } else {
-          type.fields.push(lines[i].trim())
+          type.fields.push(lines[i].trim());
         }
       }
-      schema.types.push(type)
+      schema.types.push(type);
     } else {
-      const directiveName = lines[n].split(" ")[0].slice(0, -1) //this is pretty consusing. TODO: make it clearer that you remove the : from the end of the directive name
-      const directiveType = lines[n].split(" ")[1]
+      const directiveName = lines[n].split(" ")[0].slice(0, -1); //this is pretty consusing. TODO: make it clearer that you remove the : from the end of the directive name
+      const directiveType = lines[n].split(" ")[1];
 
       const directive: Directive = {
         name: directiveName,
         type: directiveType,
-      }
+      };
       if (lines[n].includes("@index")) {
         //extract index type
-        const indexType = lines[n].split("@index(")[1].split(")")[0]
-        directive.index = indexType.split(", ")
+        const indexType = lines[n].split("@index(")[1].split(")")[0];
+        directive.index = indexType.split(", ");
       }
-      schema.directives.push(directive)
+      schema.directives.push(directive);
     }
   }
 
-  return schema
-}
+  return schema;
+};
 
 export const dqlSchemaJsonToZodSchemaString = (dqlSchema: DQLSchema) => {
   const dqlTypeToZodTypeMap = {
@@ -72,42 +90,43 @@ export const dqlSchemaJsonToZodSchemaString = (dqlSchema: DQLSchema) => {
     bool: "boolean()",
     float: "number()",
     datetime: "date()",
-  }
+  };
 
-  let zodSchemaString = `import { z } from 'zod'\n\n`
+  let zodSchemaString = `import { z } from 'zod'\n\n`;
 
   for (const type of dqlSchema.types) {
-    zodSchemaString += `const ${type.name} = z.object({\n`
+    zodSchemaString += `const ${type.name} = z.object({\n`;
 
     for (const [index, field] of type.fields.entries()) {
-      const fieldWithoutTypePrefix = field.split(".").slice(-1)[0]
+      const fieldWithoutTypePrefix = field.split(".").slice(-1)[0];
 
       const dqlType = dqlSchema.directives.find(
         (directive) => directive.name === field
-      )?.type as keyof typeof dqlTypeToZodTypeMap
+      )?.type as keyof typeof dqlTypeToZodTypeMap;
 
       if (dqlType === undefined)
         throw new Error(
           `Could not find type for field ${field} in type ${type.name}`
-        )
+        );
       else {
-        const indentation = "\t"
+        const indentation = "\t";
         const directive = dqlSchema.directives.find(
           (directive) => directive.name === field
-        )
-        const isOptional = directive?.index?.includes("term") ? false : true
-        const zodType = dqlTypeToZodTypeMap[dqlType]
-        if(zodType === undefined) throw new Error(`Could not find zod type for dql type ${dqlType}`)
+        );
+        const isOptional = directive?.index?.includes("term") ? false : true;
+        const zodType = dqlTypeToZodTypeMap[dqlType];
+        if (zodType === undefined)
+          throw new Error(`Could not find zod type for dql type ${dqlType}`);
         if (isOptional) {
-          zodSchemaString += `${indentation}${fieldWithoutTypePrefix}: z.${zodType}.optional(),\n`
+          zodSchemaString += `${indentation}${fieldWithoutTypePrefix}: z.${zodType}.optional(),\n`;
         } else
-          zodSchemaString += `${indentation}${fieldWithoutTypePrefix}: z.${zodType},\n`
+          zodSchemaString += `${indentation}${fieldWithoutTypePrefix}: z.${zodType},\n`;
       }
     }
-    zodSchemaString += `})\n\n`
+    zodSchemaString += `})\n\n`;
 
-    zodSchemaString += `type ${type.name} = z.infer<typeof ${type.name}>\n\n`
+    zodSchemaString += `type ${type.name} = z.infer<typeof ${type.name}>\n\n`;
   }
 
-  return zodSchemaString
-}
+  return zodSchemaString;
+};
